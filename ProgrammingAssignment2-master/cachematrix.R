49b9c267b31f7814578d7effeedaf42ab2e3d11b
KariBenson <benson@lynchburg.edu>
2025-07-21 12:40:23 -0400

    Update cachematrix.R
    module 2 program for R Programming

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -1,16 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## These functions work in concert to check whether there is a cached inverse matrix stored, and, if not, to store one for the matrix "x"
 
 makeCacheMatrix <- function(x = matrix()) {
 
+# set value of matrix
+  invMat <- NULL
+ 
+# get value of matrix
+ set <- function(n) {
+    # make sure invMat starts empty
+    x <<- n
+    invMat <<- NULL 
+      }
+ 
+# get matrix inverse
+  get <- function () x
+  setinvMat <- function(solve) invMat <<- solve
+
+# store matrix inverse
+  getinvMat <- function () invMat
+  list(set = set, get = get, setinvMat = setinvMat, getinvMat = getinvMat)
+
+ 
 }
-## new test here...
 
-
-## Write a short comment describing this function
+#
+# 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+# check cache 
+  n <- x$getinvMat()
+  if(!is.null(n)){
+    message("getting cached data")
+    return(n)
+  }
+# calculate if not cached  
+  data <- x$get()
+  n <- solve(data,...)
+  x$setinvMat(n)
+  n
+  
 }
